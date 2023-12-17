a32f39bac4fcc0789ec890fed03fa993040b6e20
Augusto Pires <91226967+augustop01@users.noreply.github.com>
2023-12-17 20:52:54 -0300

    FIX - limpeza de comentários obsoletos

--- src/main/java/m2/MedMngt/DB/models/IdEstoque.java
+++ src/main/java/m2/MedMngt/DB/models/IdEstoque.java
@@ -11,16 +11,4 @@ import java.util.Objects;
 public class IdEstoque implements Serializable {
     private Long cnpj;
     private Integer nroRegistro;
-
-//    public boolean seIguala(Object obj){
-//        if (this == obj) return true;
-//        if (obj == null || getClass() != obj.getClass()) return false;
-//        IdEstoque idEstoque = (IdEstoque) obj;
-//        return Objects.equals(cnpj, idEstoque.cnpj) && Objects.equals(nroRegistro, idEstoque.nroRegistro);
-//    }
-//
-//    @Override
-//    public int hashCode(){
-//        return Objects.hash(cnpj, nroRegistro);
-//    }
 }
